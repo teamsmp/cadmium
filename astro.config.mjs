7ab254e09e35c714f499685ef5173241129c9be0
Antoni <tubikardu@gmail.com>
2026-01-11 13:33:00 +0000

    ci: prepare docker-build workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.teamsmp.uk",
+
   vite: {
     plugins: [tailwindcss()],
   },
